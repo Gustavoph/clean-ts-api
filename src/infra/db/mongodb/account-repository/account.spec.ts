972b88e818a0f7749cab501b83d6cc51ebf88810
Gustavoph <gusta.coliveira07@gmail.com>
2022-09-25 14:34:03 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return an accoun on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any@mail.com',
+      password: 'hashed_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any@mail.com')
+    expect(account.password).toBe('hashed_password')
+  })
+})
