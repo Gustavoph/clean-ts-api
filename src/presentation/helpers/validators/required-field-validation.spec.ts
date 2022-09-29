5415dd61ce509f40252b0dab698d4771abf1daf5
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-09-29 09:43:49 -0300

    test: ensure MissingParamError

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
+describe('RequiredField Validation', () => {
+  it('should return MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
+})
