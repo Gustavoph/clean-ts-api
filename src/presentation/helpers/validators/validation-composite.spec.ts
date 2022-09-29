faa2eff6d32ecfc4a01f8580c26fc0516b60f49c
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-09-29 10:09:51 -0300

    test: ensure ValidationComposite return null if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,7 +4,7 @@ import { ValidationComposite } from './validation-composite'
 const makeValidation = () => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
-      return new Error()
+      return null
     }
   }
 
@@ -12,13 +12,25 @@ const makeValidation = () => {
 }
 
 const makeSut = () => {
-  return new ValidationComposite([makeValidation()])
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
   it('should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
+  })
+
+  it('should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
