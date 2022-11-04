b491303a935a32d348f4d61c32b5171ae636884e
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-11-04 11:35:15 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,10 +1,11 @@
-import { HashComparer, LoadAccountByEmailRepository, TokenGenerator } from '../../protocols'
+import { HashComparer, LoadAccountByEmailRepository, TokenGenerator, UpdateAccessTokenRepository } from '../../protocols'
 import { Authentication, AuthenticationModel } from '../add-account/db-add-account-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly hashComparer: HashComparer,
     private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
@@ -14,6 +15,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
